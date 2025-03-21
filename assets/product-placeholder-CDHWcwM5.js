ed75bfc43aef4b09e8b7a840c02c73c181bbca9c
guyong <dujcel@icloud.com>
2025-03-22 00:13:25 +0800

    fixing vite base and router base

---
+++ assets/product-placeholder-CDHWcwM5.js
@@ -0,0 +1,1 @@
+const e="/vueshop/assets/product-placeholder-DblXBoBA.jpg";export{e as p};
--- assets/product-placeholder-DLIEfcZa.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("product-placeholder-DblXBoBA.jpg",import.meta.url).href;export{e as p};
